545ccd37fab73fcfbb0e8cf263062964fb07dc73
bayarens <bay.arens@gmail.com>
2021-05-12 15:53:19 -0400

    added testing request

--- src/App.js
+++ src/App.js
@@ -4,40 +4,66 @@ import { ourData } from './fakeApi';
 import Header from './Components/Header';
 import Card from './Components/Card';
 import Greeting from './Components/Greeting';
+import { randomPhoto, carList } from './unsplashRequests';
 
 
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       selected: 'cars',
+      info: null,
+      randomImg: null,
       cars: ourData.cars,
       boats: ourData.boats
     }
   }
-  
+
   handleClick = (text) => {
     this.setState({ selected: text.toLowerCase() });
   }
 
-  createCards(){
-    return(
+  createCards() {
+    return (
       this.state[this.state.selected].map((info, idx) => (
-        <Card 
-        imgSrc={info} 
-        key={idx} 
+        <Card
+          imgSrc={info}
+          key={idx}
         />
       ))
     )
   }
 
-  render(){
+  makeRequest= async () => {
+  const response = await randomPhoto();
+  this.setState({ 
+      info: response.description || response.alt_description,
+      randomImg: response.urls.small
+    });
+  }
+
+  getCars = async () => {
+    const response = await carList();
+    this.setState({
+      cars: response
+    })
+  }
+
+  render() {
     return (
       <div className="App">
         <Greeting />
-        <Header handleClick = {this.handleClick}/>
+        <button onClick={this.getCars}>Testing request</button>
+        <div>
+          <h3>random photo description</h3>
+          <div>
+            {this.state.info}
+            <img src={this.state.randomImg} />
+          </div>
+        </div>
+        <Header handleClick={this.handleClick} />
         <section className="photos-container">
           {this.createCards()}
         </section>
---
+++ src/unsplashRequests.js
@@ -0,0 +1,27 @@
+const ACCESS_KEY = '_172BiL5qCqigNcvvr5nSYQ-WbxZzvlOiXoqbjR04A4'
+const BASE_URL = 'https://api.unsplash.com/';
+const endPoints = {
+    random: '/photos/random/'
+}
+
+export const randomPhoto = async () => {
+    const response = await fetch(`${BASE_URL}${endPoints.random}`, {
+        headers: {
+            Authorization: `Client-ID ${ACCESS_KEY}` 
+        }
+    })
+    .then(res => res.json())
+    .then(res => res);
+    return response
+}
+
+export const carList = async () => {
+    const response = await fetch(`${BASE_URL}${endPoints.random}?query=car&&count=4`, {
+        headers: {
+            Authorization: `Client-ID ${ACCESS_KEY}`,
+        }
+    })
+    .then(res => res.json())
+    .then(res => res);
+    return response;
+}
